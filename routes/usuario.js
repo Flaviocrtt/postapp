deb038f819fa29fbabb75f8688916b9a8c2e2c79
FlavioCrtt <flavio777crtt@hotmail.com>
2022-02-04 01:16:30 -0300

    Salvar usuário

--- routes/usuario.js
+++ routes/usuario.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const mongoose = require("mongoose")
+const bcrypt = require('bcryptjs');
 
 require("../models/Usuario")
 const UsuarioModel = mongoose.model("usuario")
@@ -11,16 +12,55 @@ router.get("/registro", (req, res) => {
 
 router.post("/salvar", (req, res) => {
     let erros = [];
-    if(req.body.senha != req.body.senha2){
-        erros.push({texto:"As senhas não estão iguais"})
+    if (req.body.senha != req.body.senha2) {
+        erros.push({ texto: "As senhas não estão iguais" })
     }
 
-    if(erros.length>0){
-        res.render("usuario/registro", {erros:erros, body: req.body})
-    }else{
+    if (erros.length > 0) {
+        res.render("usuario/registro", { erros: erros, body: req.body })
+    } else {
+
+        UsuarioModel.findOne({ email: req.body.email }).then((usuario) => {
+            if (usuario) {
+                req.flash("error_msg", "Usuário com esse email já existe.")
+                res.redirect("/")
+            } else {
+                var novoUsuario = new UsuarioModel({
+                    nome: req.body.nome,
+                    email: req.body.email,
+                    senha: req.body.senha
+                });
+
+                bcrypt.genSalt(10, function (err, salt) {
+                    if (err) {
+                        req.flash("error_msg", "Ocorreu um erro interno. " + err)
+                        res.redirect("/")
+                    } else {
+                        bcrypt.hash(novoUsuario.senha, salt, function (err, hash) {
+                            if (err) {
+                                req.flash("error_msg", "Ocorreu um erro interno. " + err)
+                                res.redirect("/")
+                            } else {
+                                novoUsuario.senha = hash;
+                                novoUsuario.save().then(() => {
+                                    req.flash("success_msg", "Usuário criado com sucesso")
+                                    res.redirect("/")
+                                }).catch((error) => {
+                                    req.flash("error_msg", "Ocorreu um erro interno. " + error)
+                                    res.redirect("/")
+                                })
+                            }
+                        });
+                    }
+                });
+            }
+        }).catch((error) => {
+            req.flash("error_msg", "Ocorreu um erro interno. " + error)
+            res.redirect("/")
+        })
 
     }
-    
+
 })
 
 module.exports = router
